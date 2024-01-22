9abf02119ec4d258fffcea3d390715ed5f9f2508
Ben Brinkman <ben.brinkman.school@gmail.com>
2024-01-22 17:30:21 -0500

    added data in json file

--- Statblock.js
+++ Statblock.js
@@ -30,7 +30,10 @@ var damageType = [
     "Psychic",
     "Radiant",
     "Slashing",
-    "Thunder"
+    "Thunder",
+    "Bludgeoning, Piercing, and Slashing from nonmagical attacks",
+    "Bludgeoning, Piercing, and Slashing from nonmagical attacks that aren't adamantine",
+    "Bludgeoning, Piercing, and Slashing from nonmagical attacks that aren't silvered"
 ];
 var conditionType = [
     "Blinded",
@@ -207,7 +210,10 @@ var damageResistance = [
     false, //Psychic
     false, //Radiant
     true, //Slashing
-    false //Thunder
+    false, //Thunder
+    false, //Bludgeoning, Piercing, and Slashing from nonmagical attacks
+    false, //Bludgeoning, Piercing, and Slashing from nonmagical attacks that aren't adamantine
+    false //Bludgeoning, Piercing, and Slashing from nonmagical attacks that aren't silvered
 ];
 var damageImmunity = [
     false, //Acid
@@ -222,7 +228,10 @@ var damageImmunity = [
     false, //Psychic
     false, //Radiant
     false, //Slashing
-    true //Thunder
+    true, //Thunder
+    false, //Bludgeoning, Piercing, and Slashing from nonmagical attacks
+    false, //Bludgeoning, Piercing, and Slashing from nonmagical attacks that aren't adamantine
+    false //Bludgeoning, Piercing, and Slashing from nonmagical attacks that aren't silvered
 ];
 var damageVulnerability = [
     false, //Acid
@@ -237,8 +246,30 @@ var damageVulnerability = [
     false, //Psychic
     false, //Radiant
     false, //Slashing
-    false //Thunder
+    false, //Thunder
+    false, //Bludgeoning, Piercing, and Slashing from nonmagical attacks
+    false, //Bludgeoning, Piercing, and Slashing from nonmagical attacks that aren't adamantine
+    false //Bludgeoning, Piercing, and Slashing from nonmagical attacks that aren't silvered
 ];
+
+var conditions = [
+    false, // "Blinded"
+    false, // "Charmed"
+    false, // "Deafened"
+    false, // "Exhaustion"
+    false, // "Frightened"
+    false, // "Grappled"
+    false, // "Incapacitated"
+    false, // "Invisible"
+    false, // "Paralyzed"
+    false, // "Petrified"
+    false, // "Poisoned"
+    false, // "Prone"
+    false, // "Restrained"
+    false, // "Stunned"
+    false  // "Unconscious"
+];
+
 var additionalDamageResistance = [
     "<span class = 'pink'>Damage dealt by hexed creatures</span>"
 ];
@@ -335,13 +366,78 @@ var savingThrowAbilities = [
     "Legendary Counter"
 ];
 
+var features = [
+    {
+        type: "trait",
+        header: "<span class='green'>Blood Hex of the Direlord.</span>",
+        description: "Azoriandas is bound by a blood oath to kill Tiamat. If she is killed by anyone else or if he passes up an opportunity to kill her, he dies. <span class='pink'>Tiamat is considered hexed</span> by Azoriandas at all times. Azoriandas gains the following bonuses against creatures he has hexed: <ul class='text'> <li>A hexed creature cannot use Legendary Resistance or Indomitable against an effect Azoriandas triggers</li> <li>A Hexed creature has disadvantage on attack rolls against Azoriandas</li> <li>⚔ When Azoriandas hits a hexed creature, it cannot regain hitpoints until the start of his next turn</li> <li>⚔ Azoriandas crits on attack rolls of 18-20 when attacking a hexed creature</li> <li>⚔ Melee Attacks against a hexed creature deal <b><span class='red'>4d6</span></b> bonus damage</li> <li>⚔ When Azoriandas damages a hexed creature, it triggers his Bolstered Fury ability</li> <li>Azoriandas has resistance to all damage dealt by a hexed creature</li> <li>Azoriandas cannot be <b><span class='blue'>frightened</span></b> by a creature he has hexed</li>        </ul>"
+    },
+    {
+        type: "trait",
+        header: "Amphibious.",
+        description: "Azoriandas can breathe air and water."
+    },
+    {
+        type: "trait",
+        header: "Bolstered Fury.",
+        description: "When Azoriandas scores a critical hit or <span class='pink'>damages a hexed creature</span>, he gains <b><span class='red'>40</span></b> temp HP. While he has 1 or more of these temp HP, any creature that damages him takes <b><span class='red'>20</span></b> necrotic damage."
+    },
+    {
+        type: "bonusAction",
+        header: "(B) Cloud Leap",
+        description: "As a bonus action, Azoriandas can leap up to 120 in any direction."
+    },
+    {
+        type: "reaction",
+        header: "(R) Legendary Counter.",
+        description: "If a creature uses a legendary action to make an attack roll against Azoriandas or that would force him to make a saving throw, he may immediately use his reaction to move up to his movement speed, provoking no opportunity attacks. If the action was an attack roll or saving throw based on an area of effect and he is no longer in the area, he avoids the effect. If the effect is sight based, he avoids it only if he is no longer within sight. If he successfully is no longer targeted by the legendary action, he can use Direbeam directed at the creature that used the legendary action."
+    },
+    {
+        type: "reaction",
+        header: "Shards of Opportunity.",
+        description: "If a creature provokes an attack of opportunity (20ft. reach), Azoriandas can choose to automatically make an attack without using a reaction as a shard of the blade flies from its place to strike the target before returning to its place. <b><span class='blue'>+19</span>,<span class='red'>2d6+12</span></b> force"
+    },
+    {
+        type: "reaction",
+        header: "Indomitable (3 uses).",
+        description: "If Azoriandas fails a saving throw, he can choose to succeed instead."
+    },
+    {
+        type: "reaction",
+        header: "Magic Resistance.",
+        description: "Azoriandas has advantage on saving throws against spells and other magical effects."
+    },
+    {
+        type: "reaction",
+        header: "Action Surge.",
+        description: "Take an additional action."
+    },
+    {
+        type: "action",
+        header: "Multiattack.",
+        description: "Azoriandas makes three attacks from the options below. <span class='pink'>If he damages a creature he has hexed, he uses his Bolstered Fury</span>:<ul><li><span class='featureHeader'>The Unsettled Shards (+3). </span><span class='featureDescription'><i>20ft. </i><b><span class='blue'>+19</span>, <span ='red'>8d6+12</span></b> force(<b><span class='red'>+4d6</span></b> <span class='pink'>bonus damage on hexed creatures</span>)</span></li><li><span class='featureHeader'>Stomp. </span><span class='featureDescription'><i>5ft., one huge or smaller creature. </i><b><span class='blue'>+16</span>, <span class='red'>5d10+9</span></b> bludgeoning. The target must succeed on a DC22 Dex save or have the <b><span class='blue'>prone</span></b> condition. Until Azoriandas moves or uses Stomp again, the creature has the <b><span class='blue'>restrained</span></b> condition. The restrained creature or another creature within 5ft can use an action to make a DC22 Strength check, on a success they relocate to an unoccupied space within 5 feet of Azoriandas and end the restrained condition. </span></li></ul>"
+    },
+    {
+        type: "action",
+        header: "(A) Direbeam",
+        description: "<i>(Recharge 5-6). 500ft., </i>10ft radius, DC22 dex/half <b><span class='red'>12d8</span></b> force, on failure <b><span class='blue'>stunned</span></b> until end of next turn. The shards of the diresword spin out of place and swirl around as a black cloud gathers at the base, drawing the pieces inward. As they unite in the center, a dark beam of purple energy ripples streaks out forming a laser that focuses for a second before sputtering out, leaving the shards to snap back into place amidst a cloud of sulphuric smelling smoke."
+    },
+    {
+        type: "action",
+        header: "(A) Shard Scatter",
+        description: "<i>(Recharge 5-6). 120/350 6 targets </i><b><span class='blue'>+18</span>, <span class='red'>4d12+11</span></b> force, DC22 Con save or <span class='pink'>hexed</span> until end of Azoriandas’s next turn."
+    }
+];
+
 var prof = 7;
 
 var maxHealth = 600;
 var AC = "18 (natural armour)";
-//var speed = "50ft., swim 50ft.";
-var name = "Azoriandas"
-var type = "Gargantuan giant, chaotic good"
+var creatureName = "Azoriandas"
+
+
+
+
 
 //calculated values
 var abilityMod = [
@@ -380,17 +476,63 @@ var skill = [
     0, //Stealth
     0, //Survival
 ];
+function fetchJson() {
+    fetch('/creature.json')
+        .then(
+            function (response) {
+                return (response.json());
+            })
+        .then(function (data) {
+            console.log(data);
+            // var _data = jsonData.item[0];
 
+            // Setting variables based on the JSON values
+            abilityScore = data.abilityScore;
+            saveProficiencies = data.saveProficiencies;
+            damageResistance = data.damageResistance;
+            damageImmunity = data.damageImmunity;
+            damageVulnerability = data.damageVulnerability;
+            additionalDamageResistance = data.additionalDamageResistance;
+            additionalDamageImmunity = data.additionalDamageImmunity;
+            additionalDamageVulnerability = data.additionalDamageVulnerability;
+            additionalConditionImmunity = data.additionalConditionImmunity;
+            skillProficiency = data.skillProficiency;
+            skillExpertise = data.skillExpertise;
+            sense = data.sense;
+            creature = data.creature;
+            alignment = data.alignment;
+            size = data.size;
+            speed = data.speed;
+            languageKnown = data.languageKnown;
+            savingThrowAbilities = data.savingThrowAbilities;
+            prof = data.prof;
+            maxHealth = data.maxHealth;
+            AC = data.AC;
+            creatureName = data.name;
+            type = data.type;
+            features = data.features;
+            // cards[i].getElementsByClassName("cardHeader")[0].innerHTML =data.item[i].name;
+            console.log(creatureName);
+            reloadStatblock();
 
+        });
+}
 
 window.addEventListener('load', function () {
+    fetchJson();
+    // reloadStatblock();
+});
+
+function reloadStatblock() {
     //this will update ability scores, mods, and saves
     calculateabilityMod();
 
     writeValue("type", sizeType[size] + " " + creatureType[creature] + ", " + alignmentType[alignment]);
     writeValue("AC");
     writeValue("prof");
-    writeValue("name");
+    writeValue("creatureName");
+    console.log(creatureName);
+
     //    writeValue("type");
     writeValue("maxHealth");
     writeValue("savingThrows", savingThrowAbilities)
@@ -402,7 +544,65 @@ window.addEventListener('load', function () {
     calculateFeatureList("Immunity", "condition");
     calculateFeatureList("Known", "language");
     calculateSpeed();
-});
+    writeFeatures();
+}
+
+function writeFeatures(){
+    
+    var traitDiv = document.getElementById("trait");
+    var bonusActionDiv = document.getElementById("bonusAction");
+    var reactionDiv = document.getElementById("reaction");
+    var actionDiv = document.getElementById("action");
+    traitDiv.innerHTML = "";
+    bonusActionDiv.innerHTML = "";
+    reactionDiv.innerHTML = "";
+    actionDiv.innerHTML = "";
+
+    hideUnusedSections("trait");
+    hideUnusedSections("bonusAction");
+    hideUnusedSections("reaction");
+    hideUnusedSections("action");
+    hideUnusedSections("legendary");
+
+    features.forEach(function (item) {
+        createFeatureBlock(item.type, item.header, item.description)
+    });
+}
+
+function hideUnusedSections(section){
+    var isUsed = false;
+    features.forEach(function (item) {
+        isUsed = item.type == section && isUsed != true?true:isUsed;
+    });
+    if(!isUsed){
+        document.getElementById(section).hidden = true;
+        document.getElementById(section+"Title").hidden = true;
+    }else{
+        document.getElementById(section).hidden = false;
+        document.getElementById(section+"Title").hidden = false;   
+    }
+}
+
+function createFeatureBlock(type, header, description) {
+    var parentDiv = document.getElementById(type);
+
+    var featureDiv = document.createElement("div");
+    featureDiv.classList.add("featureBlock");
+
+    var headerSpan = document.createElement("span");
+    headerSpan.classList.add("featureHeader");
+    headerSpan.innerHTML = header + " ";
+
+    var descriptionSpan = document.createElement("span");
+    descriptionSpan.classList.add("featureDescription");
+    descriptionSpan.innerHTML = description;
+
+    featureDiv.appendChild(headerSpan);
+    featureDiv.appendChild(descriptionSpan);
+    parentDiv.appendChild(featureDiv);
+    console.log("test");
+}
+
 function calculateabilityMod() {
     abilityScore.forEach(function (item, index) {
         //calculate modifier from score
